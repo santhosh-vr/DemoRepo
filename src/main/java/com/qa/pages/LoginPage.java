1e05c7e422cbcdb8e9bb3a03a5cf2db41ec16d85
santhosh-vr <vrsan28@gmail.com>
2020-04-21 18:51:53 +0530

    Added signup page feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -5,4 +5,8 @@ public class LoginPage {
 	public void loginPage() {
 		System.out.println("Login Page");
 	}
+	
+	public void loginPage1() {
+		System.out.println("Login Page(Negative test case)");
+	}
 }
---
+++ src/main/java/com/qa/pages/SignupPage.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class SignupPage {
+	
+	public void signup() {
+		System.out.println("Sign Up page Test");
+	}
+
+}
